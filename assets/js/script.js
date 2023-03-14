1f70adc958756766f8a8755324a7de53720eb7f6
Inotila <inoati1991@gmail.com>
2023-03-14 18:58:24 +0000

    Added a scroll to top btn and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,16 +26,16 @@ showLessBtn.addEventListener("click", function (event) {
 
 // scroll to top btn
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let goToTopBTN = document.getElementById("top-btn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    goToTopBTN.style.display = "unset";
   } else {
-    mybutton.style.display = "none";
+    goToTopBTN.style.display = "none";
   }
 }
 
